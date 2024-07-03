12fde059dedcb4b0304e347827e035b243d2b986
Legacynical <legacynical@gmail.com>
2024-07-03 16:40:50 -0700

    add collision tests

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 var positionA: Vector2
 var player_direction: Vector2
-const speed = 200
+const SPEED = 300
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,6 +14,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
+
+	
 	if Input.is_action_just_pressed("left-mouse"):
 		positionA = get_global_mouse_position()
 		
@@ -22,5 +24,18 @@ func _process(delta):
 		print(positionA)	
 		print(get_global_mouse_position())
 		player_direction = Vector2(positionA - get_global_mouse_position()).normalized()
-		print(player_direction)
-	position += player_direction * speed * delta
+		print(player_direction)	
+	
+
+	position += player_direction * SPEED * delta
+
+	# Using move_and_collide.
+	var collision1 = move_and_collide(velocity * delta)
+	if collision1:
+		print("I collided with ", collision1.get_collider().name)
+
+	# Using move_and_slide.
+	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision2 = get_slide_collision(i)
+		print("I collided with ", collision2.get_collider().name, " ", i)
