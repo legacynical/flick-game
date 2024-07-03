35d9a88ac3328de12efea882ae9c0d76efea4161
Legacynical <legacynical@gmail.com>
2024-07-03 13:03:14 -0700

    add basic mouse click drag release movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 
+var positionA: Vector2
+var player_direction: Vector2
+const speed = 200
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,9 +15,12 @@ func _ready():
 func _process(delta):
 	
 	if Input.is_action_just_pressed("left-mouse"):
-		var positionA = get_global_mouse_position()
-		print(positionA)	
+		positionA = get_global_mouse_position()
+		
 	
 	if Input.is_action_just_released("left-mouse"):
-		var positionB = get_global_mouse_position()
-		print(positionB)
+		print(positionA)	
+		print(get_global_mouse_position())
+		player_direction = Vector2(positionA - get_global_mouse_position()).normalized()
+		print(player_direction)
+	position += player_direction * speed * delta
